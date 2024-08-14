b759440194570488d78db89b2d28c489e01b65bc
teejcoder <96551689+teejcoder@users.noreply.github.com>
2024-08-14 13:36:09 +1000

    chore: Add CSS file for centering text

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
 from flask import Flask, request, jsonify
+import flask
+from flask import Flask, request, jsonify
+import flask
+import requests
 
 app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/')
 def hello_flask():
+    return flask.render_template("hello.html")
 
-    return "Hello, Flask!"
-
-@app.route('/api')
-def api_route():
-    print(2 + 2)
-    return "Hello, Flask! this is the /api route"
-
-@app.route('/market-data')
-def market_data(url,r):
-    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo'
-    r = requests.get(url)
-    data = r.json()
-    return jsonify(data)
-
+@app.route('/get-market-data')
+def get_market_data():
+    response = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=QLYRUT52O4STJNRG')
+    return jsonify(response.json())
+    
 
 @app.route('/get-user/<user_id>')
 def get_user(user_id):
@@ -32,14 +29,9 @@ def get_user(user_id):
     extra = request.args.get("extra")
     if extra:
         user_data["extra"] = extra
-    
     return jsonify(user_data), 200
 
-@app.route("/create-user", methods=["POST"])
-def create_user():
-    data = request.get_json()
-    
-    return jsonify(data), 201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
