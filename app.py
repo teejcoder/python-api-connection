7c4f82fa20e90f8950912838d9656b62a07af215
teejcoder <96551689+teejcoder@users.noreply.github.com>
2024-08-14 17:34:24 +1000

    removed old routes in app.py

--- app.py
+++ app.py
@@ -16,20 +16,6 @@ def get_market_data():
     filtered_data = {day: data['Time Series (Daily)'][day] for day in last_10_days}
     return jsonify(filtered_data)
 
-@app.route('/get-user/<user_id>')
-def get_user(user_id):
-    user_data = {
-        "user_id": user_id,
-        "name": "John Doe",
-        "email": "johndoe@mail.com"
-    }
-    
-    extra = request.args.get("extra")
-    if extra:
-        user_data["extra"] = extra
-    return jsonify(user_data), 200
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
