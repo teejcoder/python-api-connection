8d0449793a3b84ea2ae624c9d0544d0e0e52a9c8
teejcoder <96551689+teejcoder@users.noreply.github.com>
2024-08-16 20:33:50 +1000

    add dashboard route and template

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def hello_flask():
     return flask.render_template("hello.html")
 
+@app.route('/dashboard')
+def dashboard():
+    return flask.render_template("dashboard.html")
+
 @app.route('/get-market-data')
 def get_market_data():
     response = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=QLYRUT52O4STJNRG')
